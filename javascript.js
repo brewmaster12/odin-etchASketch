061778adee9007632a99d658ebe3150887867e58
brewmaster12 <seanywg@gmail.com>
2024-10-05 17:42:11 +0100

    Create hover effect that changes square's color

--- javascript.js
+++ javascript.js
@@ -5,5 +5,10 @@ let userInput = 16;
 for (i = 0; i < (userInput**2); i++) {
     const square = document.createElement("div");
     square.style.cssText = `width: ${500/userInput}px; aspect-ratio: 1/1; outline: 1px solid black;`
+    
+    square.addEventListener("mouseenter", () => {
+        square.style.background = "red";
+    })
+
     container.appendChild(square);
 }
