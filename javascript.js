576f72c424ba661dbd4505d975545f8142ac8455
brewmaster12 <seanywg@gmail.com>
2024-10-06 22:47:57 +0100

    Make reset function empty the container

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 const container = document.querySelector("#container");
-container.style.cssText = "display: flex; flex-wrap: wrap";
 
 function generateSquares(userInput) {
+    
     for (i = 0; i < (userInput**2); i++) {
         const square = document.createElement("div");
         square.style.cssText = `width: ${500/userInput}px; aspect-ratio: 1/1; outline: 1px solid black;`
@@ -17,6 +17,6 @@ generateSquares(16);
 const reset = document.querySelector("button");
 reset.addEventListener("click", () => {
     let userInput = prompt("Enter number of squares per side (max. 100)");
-
+    container.textContent = "";
     generateSquares(userInput);
 })
